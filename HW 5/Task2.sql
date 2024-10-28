459e37ef05889b480a6f82c369b42f816a7808da
YaqubRahman1 <Yaqub.Rahman@city.ac.uk>
2024-10-28 12:38:12 +0000

    Add files via upload

---
+++ HW 5/Task2.sql
@@ -0,0 +1,33 @@
+--1
+
+SELECT SUM(bill_total)
+    AS 'Income'
+	FROM restbill;
+
+--2
+
+SELECT SUM(bill_total)
+AS 'Feb Income'
+FROM restbill
+WHERE bill_date LIKE '1602%';
+
+--3
+
+SELECT AVG(bill_total)
+AS 'Average Bill'
+FROM restbill;
+
+--4
+
+SELECT MAX(no_of_seats) AS 'Max', MIN(no_of_seats) AS 'MIN', AVG(no_of_seats) AS 'Avg'
+FROM restrest_table
+WHERE room_name='Blue';
+
+--5
+
+SELECT COUNT(table_no)
+AS 'Distinct Tables'
+FROM restbill
+WHERE waiter_no='4,2';
+
+
