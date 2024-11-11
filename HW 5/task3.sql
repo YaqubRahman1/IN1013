e2f79725cb8d28511bd2a6742eac766ed1f2b93d
YaqubRahman1 <Yaqub.Rahman@city.ac.uk>
2024-11-11 20:20:41 +0000

    Update task3.sql

--- HW 5/task3.sql
+++ HW 5/task3.sql
@@ -1,10 +1,12 @@
 --1
+
 SELECT first_name, surname
 FROM reststaff
 INNER JOIN restbill ON reststaff.staff_no = restbill.waiter_no
 WHERE cust_name = 'Tanya Singh';
 
 --2
+
 SELECT room_date
 FROM restroom_management
 WHERE headwaiter ='10'
@@ -12,11 +14,13 @@ AND room_name = 'Green'
 AND room_date LIKE '16%';
 
 --3
+
 SELECT first_name, surname
 FROM reststaff
 WHERE headwaiter IN (SELECT headwaiter FROM reststaff WHERE first_name = 'Zoe' AND surname = 'Ball');
 
 --4
+
 SELECT DISTINCT cust_name, bill_total, reststaff.first_name, reststaff.surname
 FROM restbill
 INNER JOIN reststaff
@@ -24,6 +28,7 @@ ON waiter_no = staff_no
 ORDER BY bill_total DESC;
 
 --5
+
 SELECT first_name, surname
 FROM reststaff
 INNER JOIN restbill
@@ -32,6 +37,7 @@ WHERE bill_no = '14' OR bill_no = '17';
 
 
 --6
+
 SELECT DISTINCT
     w.first_name, w.surname, h.first_name, h.surname 
 FROM reststaff w
